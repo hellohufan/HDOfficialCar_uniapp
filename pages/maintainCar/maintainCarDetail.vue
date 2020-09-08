bb35953ef5e9d62b745e058ac40c1e6a61875096
hellohufan <hellohufan@gmail.com>
2020-09-08 08:56:36 +0800

    complete maitaincarlist page

---
+++ pages/maintainCar/maintainCarDetail.vue
@@ -0,0 +1,264 @@
+<template>
+	<view class="topBackView">
+		<view class="section">
+			<view class="alignment_justify">
+				<view>
+					<text>当前节点：</text>
+					<text style="color: #f4643e;">未审批</text>
+				</view>
+				<text style="color: #707070;">维修单号：47890</text>
+			</view>
+			<view class="uni-list">
+				<block v-for="data in listData" :key="index">
+					<view class="uni-list-cell" @click="goDetail(value)"> 
+						<view class="alignment_left">
+							<image class="schedule_icon"></image>
+							<view class="schedule_time">2020-10-22 18:35</view>
+						</view>
+						<view class="alignment_left">
+							<view class="schedule_line"></view>
+							<text>未审批</text>
+							<view class="alignment_left">
+								<view></view>
+							</view>
+						</view>
+					</view>
+				</block>
+			</view>
+		</view>
+		<view class="section">
+			<view class="title_left">维修单详情</view>
+			<view class="alignment_left">
+				<block v-for="label in labels">
+					<view class="lable">{{label}}</view>
+				</block>
+			</view>
+			<view class="line_seperate"></view>
+			<view class="alignment_left">
+				<text class="alignment_left margin_left_section">送修时间:</text>
+				<view class="schedule_time">2020-12-23 10:39</view>
+			</view>
+			<view class="corner_gray_back">
+				<view class="alignment_left margin_left">
+					<text class="">维修厂：</text>
+					<view class="">福州市三牛汽车有限公司</view>
+				</view>
+				<view class="line_seperate"></view>
+				<block v-for="data in maitainDetailTitles">
+					<view class="alignment_left">
+						<view class="row_title margin_left">{{data}}</view>
+						<view class="row_title">吴三桂</view>
+					</view>
+				</block>
+			</view>
+			<view class="remark">备注信息：开卷考试拉法基到拉萨 就开始大量分解肯定萨拉 卡上了酒店费打法打。</view>
+		</view>
+		<view class="section">
+			<view class="title_left">材料费详情</view>
+			<view class="line_table_seperate"></view>
+			<view class="table_margin">
+				<scroll-view class="scroll-view_H table_margin" scroll-x="true" @scroll="scroll">
+					<block v-for="title in materialFeeDetailsTitle">
+						<view class="title_table">{{title}}</view>
+						<view id="demo1" class="scroll-view-item_H uni-bg-red">A</view>
+						<view id="demo2" class="scroll-view-item_H uni-bg-green">B</view>
+						<view id="demo3" class="scroll-view-item_H uni-bg-blue">C</view>
+						<view class="line_table_seperate"></view>
+					</block>
+				</scroll-view>
+			</view>	
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '哇哈哈'
+			}
+		},
+		onLoad() {
+			this.listData = ["123", "413"];
+			this.labels = ["五菱", "小型客车", "定点维修"];
+			this.maitainDetailTitles = ["送修司机：", "车牌号码：", "型号：", "送修里程数：", "报牌日期：", "报修项目：", "故障描述"];
+			this.materialFeeDetailsTitle = ["配件或材料名称", "型号", "配件类型", "供货商", "联系电话", "数量", "单位", "进货单价(元)", "加价比例", "合计", "状态", "材料金额合计"];
+			this.materialNames = ["机油格", "滤清器", "机油", "雨刮器", "阀门", "空调滤清器"];
+		},
+		methods: {
+	
+		}
+	}
+</script>
+
+<style>
+	/* #ifndef APP-NVUE */
+	page {
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		background-color: #efeff4;
+		min-height: 100%;
+		height: auto;
+	}
+	/* #endif */
+	view {
+		font-size: 35rpx;
+		line-height: inherit;
+	}
+	.section{
+		background-image: url(../../static/cell_back.png);
+		background-repeat: no-repeat;
+		height: flex;
+		width: 100%;
+		padding-bottom: 40rpx;
+		background-color: #00000000;
+		background-size: 100% 100%;
+	}
+	.margin_left{
+		margin-left: 20rpx;
+	}
+	.margin_left_section{
+		margin-left: 40rpx;
+	}
+	.alignment_justify{
+		margin-left: 40rpx;
+		margin-right: 40rpx;
+		margin-bottom: 20rpx;
+		padding-top: 20rpx;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.alignment_left{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	.title_left{
+		padding-top: 40rpx;
+		padding-bottom: 20rpx;
+		margin-left: 40rpx;
+	}
+	.schedule_icon{
+		background-image: url(../../static/icon_schedule.png);
+		background-size: contain;
+		background-repeat: no-repeat;
+		margin-left: 60rpx;
+		margin-right: 10rpx;
+		width: 32rpx;
+		height: 32rpx;
+	}
+	.schedule_time{
+		margin-left: 10rpx;
+		color: #808080;
+	}
+	.schedule_line{
+		width: 1rpx;
+		height: 80rpx;
+		margin-left: 75rpx;
+		margin-right: 30rpx;
+		background-color: #e3e3e3;
+	}
+	.lable{
+		background-color: #f4643e;
+		color: #ffffff;
+		padding-left: 16rpx;
+		padding-right: 16rpx;
+		padding-bottom: 4rpx;
+		padding-top: 2rpx;
+		margin-right: 0rpx;
+		margin-left: 50rpx;
+		margin-bottom: 10rpx;
+		border-radius: 6rpx;
+		white-space: nowrap;
+	}
+	.line_table_horizatal{
+		background-color: #cccccc;
+		height: 1px;
+		margin-left: 10rpx;
+		margin-right: 10rpx;
+		margin-top: 0rpx;
+		margin-bottom: 10rpx;
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		content: "";
+	}
+	.line_seperate{
+		background-color: #cccccc;
+		height: 1px;
+		margin-left: 20rpx;
+		margin-right: 20rpx;
+		margin-top: 10rpx;
+		margin-bottom: 10rpx;
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		content: "";
+	}
+	.corner_gray_back{
+		border-radius: 10rpx;
+		padding-top: 20rpx;
+		padding-bottom: 20rpx;
+		background-color: #f5f5f5;
+		height: flex;
+		margin-left: 40rpx;
+		margin-right: 40rpx;
+		margin-top: 20rpx;
+	}
+	.row_title{
+		margin-bottom: 15rpx;
+		
+	}
+	.table_margin{
+		margin-left: 12rpx;
+		margin-right: 37rpx;
+	}
+	.remark{
+		margin-left: 40rpx;
+		margin-right: 40rpx;
+		margin-top: 20rpx;
+		margin-bottom: 20rpx;
+		color: #707070;
+	}
+	.scroll-view_H {
+		white-space: nowrap;
+	}
+	.scroll-view-item_H {
+		display: inline-block;
+		width: 260rpx;
+		height: 60rpx;
+		line-height: 60rpx;
+		text-align: center;
+		font-size: 35rpx;
+	}
+	.title_table{
+		display: inline-block;
+		background-color: #eeeeee;
+		width: 260rpx;
+		text-align: end;
+		padding-top: 6rpx;
+		padding-bottom: 6rpx;
+		padding-right: 10rpx;
+	}
+	.line_table_seperate{
+		background-color: #cccccc;
+		height: 1rpx;
+		margin-right: 25rpx;
+		margin-top: 0rpx;
+		margin-bottom: 0rpx;
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		content: "";
+	}
+	.lineTable_vertical{
+		background-color: #cccccc;
+		height: flex;
+		margin-left: 0rpx;
+		margin-right: 0rpx;
+		margin-top: 0rpx;
+		margin-bottom: 0rpx;
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		content: "";
+	}
+</style>
---
+++ pages/maintainCar/maintainCarList.vue
@@ -0,0 +1,304 @@
+<template>
+	<view class="topBackView">
+		<view style="background-color: #00000000;">
+			<uni-search-bar radius="100" placeholder="请输入您要查找的司机名字" clearButton="auto" cancelButton="auto" @input="input" @cancel="cancel"  @confirm="search" />
+		</view>
+		<view class="uni-list">
+			<block v-for="(value, index) in listData" :key="index">
+				<view class="uni-list-cell" @click="goDetail(value)"> 
+					<view class="contentView">
+						<view class="carNo">
+							<text>车牌号：{{value.carNo}}</text>
+							<view class="requestStatus">{{value.levelName}}</view>
+						</view>
+						<view class="line"></view>
+						<view class="cellRowItem">
+							<image class="cellRowItemIcon" src="../../static/repair_factory.png"></image>
+							<text class = "overflowSuspension">维修厂: {{value.enquiryMaintenNames}}</text>
+						</view>
+						<view class="cellRowItem">
+							<image class="cellRowItemIcon" src="../../static/repair_type.png"></image>
+							<text>维修类型: {{value.repairTypeName}}</text>
+						</view>
+						<view class="cellRowItem">
+							<image class="cellRowItemIcon" src="../../static/user_car_name.png"></image>
+							<text class="darkGray overflowSuspension">司机: {{value.driverName}}</text>
+							<text class="phone" @click.stop="telephone({phone:value.concatPhone})">{{value.concatPhone}}</text>
+						</view>
+						<view class="cellRowItem">
+							<image class="cellRowItemIcon" src="../../static/time.png"></image>
+							<text class="darkGray">送修时间: {{value.repairTime}}</text>
+						</view>
+					</view>
+				</view>
+			</block>
+		</view>
+		<uni-load-more :status="status" :icon-size="16" :content-text="contentText" />
+	</view>
+</template>
+
+<script>
+	import uniLoadMore from '@/components/uni-load-more/uni-load-more.vue';
+	var dateUtils = require('../../common/util.js').dateUtils;
+	
+	export default {
+		components: {
+			uniLoadMore
+		},
+		data() {
+			return {
+				listData: [],
+				last_id: '',
+				reload: false,
+				status: 'more',
+				page: 1,
+				adpid: '',
+				contentText: {
+					contentdown: '上拉加载更多',
+					contentrefresh: '加载中',
+					contentnomore: '没有更多'
+				}
+			};
+		},
+		onLoad() {
+			console.log("[Index]:onLoad");
+			this.adpid = this.$adpid;
+			this.getList();
+		},
+		onPullDownRefresh() {
+			this.reload = true;
+			this.last_id = '';
+			this.getBanner();
+			this.getList();
+		},
+		onReachBottom() {
+			this.status = 'more';
+			this.getList();
+		},
+		methods: {
+			search(res) {
+				uni.showToast({
+					title: '搜索：' + res.value,
+					icon: 'none'
+				})
+			},
+			input(res) {
+				this.searchVal = res.value
+			},
+			cancel(res) {
+				// uni.showToast({
+				// 	title: '点击取消，输入值为：' + res.value,
+				// 	icon: 'none'
+				// })
+			},
+			
+			getList() {
+				console.log("getList");
+				var data = {
+					flag: 'doAuditing',
+					page: this.page,
+					rows: '5'
+				};
+				var serverUrl = 'http://gcb.fjhtxl.net:81/mywork/repairapprove/queryOrderPage.do';
+				if (this.last_id) {
+					//说明已有数据，目前处于上拉加载
+					this.status = 'loading';
+					console.log(data);
+				}
+				uni.request({
+					url: serverUrl,
+					header: {
+						ACCESS_TOKEN:'Kst98wdUcjug0XzyLhQBIWgquhjuJyxJIorGWdQ!_R4CjA===@36FED37257034F398FD7E2568CEF1AD3',
+						},
+					data: data,
+					method: "GET",
+					success: data => {
+						console.log("data.data = " + data.data);
+						console.log("data.statusCode = " + data.statusCode)
+						if (data.statusCode != 200) {
+							uni.showToast({
+								title:"网络请求失败",
+								icon:"none"
+							})
+							return;
+						}
+						console.log("1111111");
+						var list = data.data.rows;
+						if (list.length == 0){
+							this.status = 'contentnomore'
+							return;
+						}
+						this.listData = this.reload? ist: this.listData.concat(list);
+						this.last_id = list[list.length - 1].id;
+						this.page++;
+						console.log("22222");
+						console.log("list: " + JSON.stringify(list));
+					},
+					fail: (data, code) => {
+						console.log('fail:' + JSON.stringify(data));
+					}
+				});
+			},
+			goDetail: function(e) {
+				let detail = {
+					author_name: e.author_name,
+					cover: e.cover,
+					id: e.id,
+					post_id: e.post_id,
+					published_at: e.published_at,
+					title: e.title
+				};
+				console.log(detail)
+				uni.navigateTo({
+					url: '../maintainCar/maintainCarDetail?detailDate=' + encodeURIComponent(JSON.stringify(detail))
+				});
+			},
+			setTime: function(items) {
+				var newItems = [];
+				items.forEach(e => {
+					newItems.push({
+						author_name: e.author_name,
+						cover: e.cover,
+						id: e.id,
+						post_id: e.post_id,
+						published_at: dateUtils.format(e.published_at),
+						title: e.title
+					});
+				});
+				return newItems;
+			},
+			telephone: function(phone){
+				console.log(phone.phone);
+			}
+		},
+		onBackPress() {
+			// #ifdef APP-PLUS
+			plus.key.hideSoftKeybord();
+			// #endif
+		}
+	}
+</script>
+
+<style>
+	/* 头条小程序组件内不能引入字体 */
+	/* #ifdef MP-TOUTIAO */
+	@font-face {
+		font-family: uniicons;
+		font-weight: normal;
+		font-style: normal;
+		src: url('~@/static/uni.ttf') format('truetype');
+	}
+	
+	/* #endif */
+	
+	/* #ifndef APP-NVUE */
+	page {
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		background-color: #efeff4;
+		min-height: 100%;
+		height: auto;
+	}
+	
+	view {
+		font-size: 35rpx;
+		line-height: inherit;
+	}
+	.darkGray{
+		color: #707070;
+	}
+	.contentView{
+		width: 100%;
+		background-color: #00000000;
+		background-image: url(../../static/cell_back.png);
+		height: 440rpx;
+		background-size: 100% 100%;
+		background-repeat: no-repeat;
+	}
+	.carNo {
+		margin-left: 40rpx;
+		padding-top: 40rpx;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.line{
+		margin-left: 40rpx;
+		margin-right: 40rpx;
+		margin-top: 20rpx;
+		top: 20rpx;
+		height: 1rpx;
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+	.requestStatus{
+		width: flex;
+		height: 50rpx;
+		padding-left: 10rpx;
+		padding-right: 10rpx;
+		text-align: center;
+		line-height: 50rpx;
+		font-size: 30rpx;
+		border-radius: 8rpx;
+		align-content: center;
+		margin-right: 40rpx;
+		color: #ffffff;
+		background-color: #f4643e;
+	}
+	.cellRowItem{
+		margin-left: 40rpx;
+		margin-right: 40rpx;
+		padding-top: 20rpx;
+		display: flex;
+		height: 50rpx;
+		flex-direction: row;
+		line-height: 50rpx;
+		justify-content: flex-start;
+	}
+	.searchView {
+		/* display: block; */
+		padding: 30rpx;
+		color: #3b4144;
+		background-color: #ffffff00;
+		height: auto;
+		/* line-height: 2rpx; */
+	}
+	.phone{
+		margin-left: 60rpx;
+		color: #707070;
+	}
+	.phone:hover {
+		color: #f4643e;
+	}
+	.cellRowItemIcon{
+		margin-left: 40;
+		margin-top: 8rpx;
+		margin-right: 10rpx;
+		width: 32rpx;
+		height: 32rpx;
+	}
+	/* #endif */
+	
+	.uni-media-list-body {
+		height: auto;
+		justify-content: space-around;
+	}
+	
+	.uni-media-list-text-top {
+		height: 248rrpx;
+		font-size: 56rrpx;
+		overflow: hidden;
+	}
+	.overflowSuspension{
+		white-space: nowrap;
+		text-overflow: 
+		ellipsis;overflow: hidden;
+	}
+	.uni-media-list-text-bottom {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+</style>
